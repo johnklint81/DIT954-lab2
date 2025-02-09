955599f73a74faf037659cf9b40578fb807a1ef0
John Klint <gusjohn25@student.gu.se>
2025-02-09 18:22:47 +0100

    Part 2: comments and a fix

--- Ramp.java
+++ Ramp.java
@@ -1,26 +1,29 @@
-import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.ArrayDeque;
 
 public class Ramp {
     private Truck owner;    // the transporter that the ramp is installed on
     private boolean down = false;   // is the ramp down or not?
     private final int capacity;    // how many cars can the ramp hold
-    private final Deque<Car> stack;   // container for cars
-    private double threshold = 10;  // meters
-
+    private final Deque<Car> stack;   // lifo container for cars
+    // constructor
     protected Ramp(Truck truck, int capacity) {
         this.owner = truck;
         this.capacity = capacity;
         stack = new ArrayDeque<>(capacity);
     }
+    // check if the car is close enough to be loaded to ramp
     public boolean isClose(Car car) {
         double sum = 0;
         for (int i = 0; i < 2; i++) {
             sum += Math.pow((this.owner.getCurrentPosition()[i] - car.getCurrentPosition()[i]), 2);
         }
+        // in meters, value to determine if car is close to ramp
+        double threshold = 10;
         return Math.sqrt(sum) < threshold;
     }
-    public void push(Car car) {
+    // push to stack
+    public void load(Car car) {
         if (stack.size() >= capacity) {
             throw new IllegalStateException("The ramp is full. Cannot load any more cars.");
         }
@@ -29,29 +32,33 @@ public class Ramp {
         }
         stack.push(car);
     }
-    public Car pop() {
+    // pop from stack
+    public Car unload() {
         if (stack.isEmpty()) {
             throw new IllegalStateException("The ramp is empty. Cannot unload any more cars.");
         }
+        else if (!isDown()) {
+            throw new IllegalStateException("The ramp is not down");
+        }
         return stack.pop();
     }
-
+    // peek on top of stack
     public Car peek() {
         return stack.peek();
     }
+    // is ramp full?
     public boolean isFull() {
         return stack.size() == capacity;
     }
-
+    // is ramp empty?
     public boolean isEmpty() {
         return stack.isEmpty();
     }
-
-
+    // is ramp down?
     public boolean isDown() {
         return down;
     }
-
+    // lower ramp
     protected void lower() {
         if (owner.getCurrentSpeed() == 0) {
             if (this.isDown()) {
@@ -63,20 +70,18 @@ public class Ramp {
                                             "when the transporter is not moving.");
         }
     }
-
+    // raise ramp
     protected void raise() {
         if (!this.isDown()) {
             System.out.println("The ramp is already up.");
         }
         this.down = false;
     }
-    public void setDown(boolean down) {
-        this.down = down;
-    }
-
+    // get the truck the ramp is installed to
     public Truck getOwner() {
         return owner;
     }
+    // get number of cars the ramp can store
     public int getCapacity() {
         return capacity;
     }
